bde1839ec07a1639497bd2fec741e21b61c82f95
lsbloo <osvaldo_cavalcanti_filho@hotmail.com>
2019-07-22 02:12:52 -0300

    login_app

--- front-end/cardeneta-app/src/app/app.module.ts
+++ front-end/cardeneta-app/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashAppComponent } from './dash-app/dash-app.component';
+import { LoginAppComponent } from './login-app/login-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashAppComponent,
+    LoginAppComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,LoginAppComponent,DashAppComponent]
 })
 export class AppModule { }
---
+++ front-end/cardeneta-app/src/app/dash-app/dash-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dash-app',
+  templateUrl: './dash-app.component.html',
+  styleUrls: ['./dash-app.component.css']
+})
+export class DashAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
